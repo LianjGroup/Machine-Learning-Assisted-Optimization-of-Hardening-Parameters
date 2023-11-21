576dc739c4e90072c2e3333385a8dcc8a6b33ef5
Fikri San Koktas <sankoktas@hotmail.com>
2023-11-21 13:26:22 +0200

    implemented loss

--- MAIN/MODELS/FINAL_MODEL.py
+++ MAIN/MODELS/FINAL_MODEL.py
@@ -32,13 +32,15 @@ X_train, X_test, y_train, y_test = train_test_split(X_data_scaled, Y_data_scaled
 
 # Define a custom neural network model
 class NeuralNetwork(nn.Module):
-    def __init__(self, input_size, hidden_size1, hidden_size2, output_size):
+    def __init__(self, input_size, hidden_size1, hidden_size2, hidden_size3, output_size):
         super(NeuralNetwork, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size1)
         self.relu1 = nn.LeakyReLU(negative_slope=0.01)
         self.fc2 = nn.Linear(hidden_size1, hidden_size2)
         self.relu2 = nn.LeakyReLU(negative_slope=0.01)
-        self.fc3 = nn.Linear(hidden_size2, output_size)
+        self.fc3 = nn.Linear(hidden_size2, hidden_size3)  # New hidden layer
+        self.relu3 = nn.LeakyReLU(negative_slope=0.01)     # New activation function
+        self.fc4 = nn.Linear(hidden_size3, output_size)
 
     def forward(self, x):
         x = self.fc1(x)
@@ -46,25 +48,32 @@ class NeuralNetwork(nn.Module):
         x = self.fc2(x)
         x = self.relu2(x)
         x = self.fc3(x)
+        x = self.relu3(x)
+        x = self.fc4(x)
         return x
 
 # Create a Skorch wrapper for the neural network with Early Stopping
 net = NeuralNetRegressor(
     module=NeuralNetwork,
     module__input_size=X_data.shape[1],
-    module__hidden_size1=64,
-    module__hidden_size2=64,
+    module__hidden_size1=256,
+    module__hidden_size2=256,
+    module__hidden_size3=128,   # New hidden layer size
     module__output_size=Y_data.shape[1],
     criterion=nn.MSELoss,
     optimizer=optim.Adam,
-    optimizer__lr=0.001,
-    max_epochs=1000,
+    optimizer__lr=0.0005,
+    max_epochs=3000,
     callbacks=[EarlyStopping(patience=20)],
     device='cuda' if torch.cuda.is_available() else 'cpu',  # Use GPU if available
 )
 
 # Train the model
-net.fit(X_train, y_train)
+history = net.fit(X_train, y_train)
+
+# Extract the training and validation losses from the history
+train_losses = history.history[:, 'train_loss']
+valid_losses = history.history[:, 'valid_loss']
 
 # Evaluate the model on the test set
 y_pred = net.predict(X_test)
@@ -99,6 +108,7 @@ with torch.no_grad():
 prediction_original = target_scaler.inverse_transform(prediction.numpy())
 
 # Print the predicted 'c-parameters'
+predicted_parameters = prediction_original[0].tolist()
 print(f'Predicted c-parameters:')
 print(prediction_original[0].tolist())
 
